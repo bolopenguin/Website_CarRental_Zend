873378b805d4ea139afc5ca944c5520ea4a4a482
Damiano Bolognini <damianobolognini1997@gmail.com>
2019-06-06 14:19:08 +0200

    merge di piddo e pincia library

--- library/App/Filter/Abstract.php
+++ library/App/Filter/Abstract.php
@@ -1,17 +1,25 @@
 <?php
-
-class App_Filter_Abstract extends Zend_Form {
-
-    public $elementDecorators = array(
+class App_Filter_Abstract extends Zend_Form
+{
+	public $elementDecorators = array(
         'ViewHelper',
-        array(array('alias1' => 'HtmlTag'), array('tag' => 'td')),
+        array(array('alias1' => 'HtmlTag'),array('tag' => 'td')),
         array('Label', array('tag' => 'td')),
         array(array('alias4' => 'HtmlTag'), array('tag' => 'td')),
-    );
-    public $buttonDecorators = array(
+        );
+        
+        public $SecondRowDecorators = array(
+        'ViewHelper',
+        array(array('alias1' => 'HtmlTag'),array('tag' => 'td')),
+        array('Label', array('tag' => 'td'),array('class'=>'row2label')),
+        array(array('alias3' => 'HtmlTag'), array('tag' => 'td')),
+        array(array('alias4' => 'HtmlTag'), array('tag' => 'tr')),
+        );
+        
+        public $buttonDecorators = array(
         'ViewHelper',
         array(array('alias1' => 'HtmlTag'), array('tag' => 'td')),
         array(array('alias2' => 'HtmlTag'), array('tag' => 'td')),
-    );
-
+    	);
 }
+
